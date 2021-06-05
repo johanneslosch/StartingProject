838b998b5acab0af7af0cd9e392c97c7fed875ae
Johannes Losch <hi@jlosch.de>
2021-06-05 12:42:07 +0200

    renamed project

--- startingProject.ps1
+++ startingProject.ps1
@@ -1,4 +1,4 @@
-param($name, $2, $3)
+param($name, $2)
 # functions
 function gitHandling {
     if($url){
@@ -10,15 +10,9 @@ function gitHandling {
     }
     cd $pName
     start https://repo.new
-
-write-host $name + ":" + $2 + ":" + $3
-
 }
 function createFolder {
     $pName = $name
-    if($3){
-        $pName = $3
-    }
     mkdir $pName
     gitHandling
 }
@@ -26,21 +20,14 @@ function createFolder {
 if($2){
     if($2 -Like "https://*.*.*"){
         $url = $2
-        if($3){
-            createFolder
-        }
         createFolder
         break
     }
     if($2 -Like "https://*.*"){
         $url = $2
-        if($3){
-            createFolder
-        }
         createFolder
         break
     }
-    #mkdir $2
     break
 }
 createFolder
